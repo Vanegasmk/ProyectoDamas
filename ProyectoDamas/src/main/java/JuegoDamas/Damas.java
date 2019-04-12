52256ed4834053d14606a2b0ba11a07eab7fa272
Kevin Vanegas <vanegasmk@gmail.com>
2019-04-11 20:39:58 -0600

    Cambio

--- ProyectoDamas/src/main/java/JuegoDamas/Damas.java
+++ ProyectoDamas/src/main/java/JuegoDamas/Damas.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package JuegoDamas;
 
 /**
  *
  * @author Dell
  */
-public class JuegoDamas {
+public class Damas {
     
 }
